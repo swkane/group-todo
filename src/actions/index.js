51b1e3ef50fa1834a5d6c0a483debb1f540e873c
Sam <swkane@indiana.edu>
2017-09-27 14:15:40 -0400

    complete list of actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,27 @@
-// Action type
+// Action Types
 export const ADD_TODO_LIST = 'ADD_TODO_LIST';
+export const FILTER_TODO_LIST = 'FILTER_TODO_LIST';
+export const ADD_TODO = 'ADD_TODO';
+export const FILTER_TODO = 'FILTER_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
 
-// Action Creator (a function that creates an action)
-
-// Action Creator w/ Action type
-
+//Action Creators
 export const addTodoList = title => {
   return {type: ADD_TODO_LIST, payload: title}
+}
+
+export const filterTodoList = title => {
+  return {type: FILTER_TODO_LIST, payload: title}
+}
+
+export const addTodo = todo => {
+  return {type: ADD_TODO, payload: todo}
+}
+
+export const filterTodo = category => {
+  return {type: FILTER_TODO, payload: category}
+}
+
+export const completeTodo = id => {
+  return {type: COMPLETE_TODO, payload: id}
 }
