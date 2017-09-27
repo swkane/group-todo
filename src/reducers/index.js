5980238a49404d6fd3dc0a7c6503411acb126e83
broleomo <brooklynnmoor@gmail.com>
2017-09-27 15:53:39 -0400

    updated first reducer again

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,8 +8,6 @@ export default const reducer =(state={},action) => {
   switch (action.type) {
     case ADD_TODO_LIST:
       return {...state, [action.payload]: []}
-    case ADD_TODO:
-      return
     default:
       return state;
   }
