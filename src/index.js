bd9379e8faaf7ef8b27c0f87dc9df4a1b1814629
Sam <swkane@indiana.edu>
2017-09-27 17:09:14 -0400

    debugged. issues with no default export. using dummy reducer rather than importing reducer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import reducer from './reducers';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 const store = createStore(
-  () => ({}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
